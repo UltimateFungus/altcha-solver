928ddc8dcc3b29393d7d961cb8ceda2c71bdb4c0
WhipLash <ultifungus@gmail.com>
2025-08-01 23:47:18 +0200

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 10000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/token", (req, res) => {
